bb1a127a92daca19ad214c95df9c14f267a9d3f1
lolzyman <markkent777@gmail.com>
2020-06-08 14:48:57 -0400

    Loot Drop Update
    Updated the Loot drops from the current monsters to work with new inventory system. Equipment Functionality not implemented yet.

---
+++ Scripts/GameEntities/Items/Inventory Items 2.0/Slime_Ball.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+
+export (PackedScene) var attack_sprite;
+export (PackedScene) var attack_collider;
+var object_id_string = "Smile_ball";
+
+func get_item_info() -> Dictionary:
+	return {
+		
+	}
